57c1d8ef9ec062334f11e5c1d032982dba397313
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-03-27 12:13:04 -0400

    update user scontroller to not include password digest in json serialization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
     def index 
         users = User.all 
-        render json: UserSerializer.new(users, include: :gardens) 
+        # render json: users.to_json(except: [:password_digest, :created_at, :updated_at])
+        render json: UserSerializer.new(users, except: :password_digest)
     end
 
     def show
         user = User.find(params[:id])
-        render json: user.to_json(include: :gardens)
+        render json: user.to_json(include: [:gardens], except: [:password_digest, :created_at, :updated_at])
     end
 
     def create
         user = User.new(user_params)
         if user.save
-            render json: UserSerializer.new(user)
+            render json: UserSerializer.new(user, except: :password_digest)
         else
             render json: {message: "failed to create"}
         end
