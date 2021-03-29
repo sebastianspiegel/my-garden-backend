6b8ecefe0be9b89bc8fed319d063f9933dee997c
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-03-29 14:34:09 -0400

    edit routes to get rid of unused ones

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'seeds#index'
-  resources :seeds
+  resources :seeds, only: [:show, :index, :new, :create]
 
   resources :users do
-    resources :gardens
+    resources :gardens, only: [:show, :new, :create]
   end
 
-  resources :gardens 
+  resources :gardens, only: [:show, :new, :create]
   
 end
