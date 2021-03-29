e5c58d14d95cd6281b4af75e362884588487a8f2
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-03-29 14:44:14 -0400

    add token to create function in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,21 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user.to_json(include: [:gardens], except: [:password_digest, :created_at, :updated_at])
     end
-
+    
     def create
         user = User.new(user_params)
         if user.save
-            render json: UserSerializer.new(user, except: :password_digest)
+            token = encode_token(user.id)
+            render json: {user: user, token: token}
         else
-            render json: {message: "failed to create"}
+            render json: {message: "failed to create user"}
         end
     end
 
     private
 
     def user_params
-        params.require(:garden).permit(:username)
+        params.require(:garden).permit(:username, :password)
     end
 
 end
