9902747c8b2d658cad67fd801a211db02f56a583
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-04 12:00:27 -0400

    update sessions controller to use jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,41 @@
 class SessionsController < ApplicationController
 
-    def login
-        user = User.find_by(username: params[:username])
-        if user && user.authenticate(params[:password])
-            token = encode_token(user.id)
-            render json: {user: UserSerializer.new(user), token: token}
+    skip_before_action :verify_authenticity_token
+
+    def create
+        user = User.find_by(username: session_params[:username])
+        if user && user.authenticate(session_params[:password])
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {
+                user: user,
+                jwt: token,
+                logged_in: true 
+            }
         else
-            render json: {error: "Incorrect Username or Password!"}
-        end
-    end
-  
-    def autologin
-        if logged_in_user
-            render json: {user: UserSerializer.new(logged_in_user)}
+            render json: {message: "Log in error. Password or username invalid."}
         end
     end
 
+    def is_logged_in?
+        if session_user
+            render json: session_user
+        else
+            render json: {message: "No user is logged in."}
+        end
+    end
+
+    def destroy
+        logout!
+        render json: {
+            status: 200,
+            logged_out: true
+        }
+    end
+
+    private
+
+    def session_params
+        params.require(:user).permit(:username, :password)
+    end
 end
-  
