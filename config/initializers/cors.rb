98d7ffba8419ccaab20c9d367ec26d1ec0fa48c8
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-30 11:41:59 -0400

    update netlify url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://my-garden-app.netlify.app/'
+    origins 'https://my-garden-app.netlify.app'
 
     resource '*',
       headers: :any,
